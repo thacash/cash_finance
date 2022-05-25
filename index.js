c22657e6453717e3b317ed89d8bb611469854293
Jonathan Grossman <51818457+JonathanGrossman@users.noreply.github.com>
2022-05-26 02:17:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,86 +1,88 @@
-module.exports = function getContractPrice(contractPrice) {
-  return parseFloat(contractPrice) * 100;
-};
+module.exports = {
+  getContractPrice: function (contractPrice) {
+    return parseFloat(contractPrice) * 100;
+  },
 
-module.exports = function getContractIncomePercent(contractPrice, stockPrice) {
-  return ((parseFloat(contractPrice) / parseFloat(stockPrice)) * 100).toFixed(
-    2
-  );
-};
+  getContractIncomePercent: function (contractPrice, stockPrice) {
+    return ((parseFloat(contractPrice) / parseFloat(stockPrice)) * 100).toFixed(
+      2
+    );
+  },
 
-module.exports = function getStockPrice(stockPrice) {
-  return parseFloat(stockPrice * 100).toFixed(2);
-};
+  getStockPrice: function (stockPrice) {
+    return parseFloat(stockPrice * 100).toFixed(2);
+  },
 
-module.exports = function getContractDays(contractExpire, contractStart) {
-  return Math.ceil(
-    (new Date(contractExpire).getTime() - new Date(contractStart).getTime()) /
-      (1000 * 3600 * 24)
-  );
-};
+  getContractDays: function (contractExpire, contractStart) {
+    return Math.ceil(
+      (new Date(contractExpire).getTime() - new Date(contractStart).getTime()) /
+        (1000 * 3600 * 24)
+    );
+  },
 
-module.exports = function getContractDaysLeft(contractExpire) {
-  return Math.ceil(
-    (new Date(contractExpire).getTime() - new Date().getTime()) /
-      (1000 * 3600 * 24)
-  );
-};
+  getContractDaysLeft: function (contractExpire) {
+    return Math.ceil(
+      (new Date(contractExpire).getTime() - new Date().getTime()) /
+        (1000 * 3600 * 24)
+    );
+  },
 
-module.exports = function getAnnualizedReturn(
-  contractExpire,
-  contractPrice,
-  contractStart,
-  stockPrice
-) {
-  return (
-    parseFloat((contractPrice / parseFloat(stockPrice)) * 100) *
-    (365 /
-      Math.ceil(
-        (new Date(contractExpire).getTime() -
-          new Date(contractStart).getTime()) /
-          (1000 * 3600 * 24)
-      ))
-  ).toFixed(2);
-};
+  getAnnualizedReturn: function (
+    contractExpire,
+    contractPrice,
+    contractStart,
+    stockPrice
+  ) {
+    return (
+      parseFloat((contractPrice / parseFloat(stockPrice)) * 100) *
+      (365 /
+        Math.ceil(
+          (new Date(contractExpire).getTime() -
+            new Date(contractStart).getTime()) /
+            (1000 * 3600 * 24)
+        ))
+    ).toFixed(2);
+  },
 
-module.exports = function getStockIncreasePercent(stockPrice, strikePrice) {
-  return (
-    100 *
-    ((parseFloat(strikePrice) - parseFloat(stockPrice)) /
-      parseFloat(stockPrice))
-  ).toFixed(2);
-};
-
-module.exports = function getSellerGenerates(
-  contractPrice,
-  stockPrice,
-  strikePrice
-) {
-  return (
-    100 *
+  getStockIncreasePercent: function (stockPrice, strikePrice) {
+    return (
+      100 *
       ((parseFloat(strikePrice) - parseFloat(stockPrice)) /
-        parseFloat(stockPrice)) +
-    parseFloat((contractPrice / parseFloat(stockPrice)) * 100)
-  ).toFixed(2);
-};
+        parseFloat(stockPrice))
+    ).toFixed(2);
+  },
 
-module.exports = function getOriginalStockValue(stockPrice) {
-  return (parseFloat(stockPrice) * 100).toFixed(2);
-};
+  getSellerGenerates: function (contractPrice, stockPrice, strikePrice) {
+    return (
+      100 *
+        ((parseFloat(strikePrice) - parseFloat(stockPrice)) /
+          parseFloat(stockPrice)) +
+      parseFloat((contractPrice / parseFloat(stockPrice)) * 100)
+    ).toFixed(2);
+  },
 
-module.exports = function getNewStockValueITM(contractPrice, strikePrice) {
-  return ((parseFloat(strikePrice) + parseFloat(contractPrice)) * 100).toFixed(
-    2
-  );
-};
+  getOriginalStockValue: function (stockPrice) {
+    return (parseFloat(stockPrice) * 100).toFixed(2);
+  },
 
-module.exports = function getSellerNetGainITM(
-  contractPrice,
-  stockPrice,
-  strikePrice
-) {
-  return (
-    ((parseFloat(strikePrice) + parseFloat(contractPrice)) * 100).toFixed(2) -
-    (parseFloat(stockPrice) * 100).toFixed(2)
-  );
+  getNewStockValueITM: function getNewStockValueITM(
+    contractPrice,
+    strikePrice
+  ) {
+    return (
+      (parseFloat(strikePrice) + parseFloat(contractPrice)) *
+      100
+    ).toFixed(2);
+  },
+
+  getSellerNetGainITM: function getSellerNetGainITM(
+    contractPrice,
+    stockPrice,
+    strikePrice
+  ) {
+    return (
+      ((parseFloat(strikePrice) + parseFloat(contractPrice)) * 100).toFixed(2) -
+      (parseFloat(stockPrice) * 100).toFixed(2)
+    );
+  },
 };
