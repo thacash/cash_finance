dcd0c2afbba1d8b20aff461ff6d5f269d469c428
Jonathan Grossman <51818457+JonathanGrossman@users.noreply.github.com>
2022-05-26 01:32:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,148 +1,86 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-import remark from "remark";
-import html from "remark-html";
-
-const postsDirectory = path.join(process.cwd(), "option-contracts");
-
-export function getSortedPostsData() {
-  const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((fileName) => {
-    const id = fileName.replace(/\.md$/, "");
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, "utf8");
-    const matterResult = matter(fileContents);
-    // let topicsArray = matterResult.data.topics.split(", ");
-
-    return {
-      id,
-      ...matterResult.data,
-      // topicsArray,
-    };
-  });
-  // Sort posts by order number in md files
-  return allPostsData.sort(({ order: b }, { order: a }) => {
-    if (a < b) {
-      return 1;
-    } else if (a > b) {
-      return -1;
-    } else {
-      return 0;
-    }
-  });
-}
-
-export function getAllPostIds() {
-  const fileNames = fs.readdirSync(postsDirectory);
-  return fileNames.map((fileName) => {
-    return {
-      params: {
-        id: fileName.replace(/\.md$/, ""),
-      },
-    };
-  });
-}
-
-export async function getPostData(id) {
-  const fullPath = path.join(postsDirectory, `${id}.md`);
-  const fileContents = fs.readFileSync(fullPath, "utf8");
-  const matterResult = matter(fileContents);
-  const processedContent = await remark()
-    .use(html)
-    .process(matterResult.content);
-  const contentHtml = processedContent.toString();
-  return {
-    id,
-    contentHtml,
-    markdown: matterResult.content,
-    ...matterResult.data,
-  };
-}
-
-export const getContractPrice = (data) => {
-  return (parseFloat(data.contract_price) * 100).toString();
+module.exports = getContractPrice = (contractPrice) => {
+  return parseFloat(contractPrice) * 100;
 };
 
-export const getContractIncomePercent = (data) => {
-  return (
-    (parseFloat(data.contract_price) / parseFloat(data.stock_price)) *
-    100
-  )
-    .toFixed(2)
-    .toString();
+module.exports = getContractIncomePercent = (contractPrice, stockPrice) => {
+  return ((parseFloat(contractPrice) / parseFloat(stockPrice)) * 100).toFixed(
+    2
+  );
 };
 
-export const getStockPrice = (data) => {
-  return parseFloat(data.stock_price * 100)
-    .toFixed(2)
-    .toString();
+module.exports = getStockPrice = (stockPrice) => {
+  return parseFloat(stockPrice * 100).toFixed(2);
 };
 
-export const getContractDays = (data) => {
+module.exports = getContractDays = (contractExpire, contractStart) => {
   return Math.ceil(
-    (new Date(data.contract_expire).getTime() -
-      new Date(data.contract_start).getTime()) /
+    (new Date(contractExpire).getTime() - new Date(contractStart).getTime()) /
       (1000 * 3600 * 24)
   );
 };
 
-export const getContractDaysLeft = (data) => {
+module.exports = getContractDaysLeft = (contractExpire) => {
   return Math.ceil(
-    (new Date(data.contract_expire).getTime() - new Date().getTime()) /
+    (new Date(contractExpire).getTime() - new Date().getTime()) /
       (1000 * 3600 * 24)
   );
 };
 
-export const getAnnualizedReturn = (data) => {
+module.exports = getAnnualizedReturn = (
+  contractExpire,
+  contractPrice,
+  contractStart,
+  stockPrice
+) => {
   return (
-    parseFloat((data.contract_price / parseFloat(data.stock_price)) * 100) *
+    parseFloat((contractPrice / parseFloat(stockPrice)) * 100) *
     (365 /
       Math.ceil(
-        (new Date(data.contract_expire).getTime() -
-          new Date(data.contract_start).getTime()) /
+        (new Date(contractExpire).getTime() -
+          new Date(contractStart).getTime()) /
           (1000 * 3600 * 24)
       ))
   ).toFixed(2);
 };
 
-export const getStockIncreasePercent = (data) => {
+module.exports = getStockIncreasePercent = (stockPrice, strikePrice) => {
   return (
     100 *
-    ((parseFloat(data.strike_price) - parseFloat(data.stock_price)) /
-      parseFloat(data.stock_price))
-  )
-    .toFixed(2)
-    .toString();
-};
-
-export const getSellerGenerates = (data) => {
-  return (
-    100 *
-      ((parseFloat(data.strike_price) - parseFloat(data.stock_price)) /
-        parseFloat(data.stock_price)) +
-    parseFloat((data.contract_price / parseFloat(data.stock_price)) * 100)
-  )
-    .toFixed(2)
-    .toString();
-};
-
-export const getOriginalStockValue = (data) => {
-  return (parseFloat(data.stock_price) * 100).toFixed(2);
-};
-
-export const getNewStockValueITM = (data) => {
-  return (
-    (parseFloat(data.strike_price) + parseFloat(data.contract_price)) *
-    100
+    ((parseFloat(strikePrice) - parseFloat(stockPrice)) /
+      parseFloat(stockPrice))
   ).toFixed(2);
 };
 
-export const getSellerNetGainITM = (data) => {
+module.exports = getSellerGenerates = (
+  contractPrice,
+  stockPrice,
+  strikePrice
+) => {
   return (
-    (
-      (parseFloat(data.strike_price) + parseFloat(data.contract_price)) *
-      100
-    ).toFixed(2) - (parseFloat(data.stock_price) * 100).toFixed(2)
+    100 *
+      ((parseFloat(strikePrice) - parseFloat(stockPrice)) /
+        parseFloat(stockPrice)) +
+    parseFloat((contractPrice / parseFloat(stockPrice)) * 100)
+  ).toFixed(2);
+};
+
+module.exports = getOriginalStockValue = (stockPrice) => {
+  return (parseFloat(stockPrice) * 100).toFixed(2);
+};
+
+module.exports = getNewStockValueITM = (contractPrice, strikePrice) => {
+  return ((parseFloat(strikePrice) + parseFloat(contractPrice)) * 100).toFixed(
+    2
+  );
+};
+
+module.exports = getSellerNetGainITM = (
+  contractPrice,
+  stockPrice,
+  strikePrice
+) => {
+  return (
+    ((parseFloat(strikePrice) + parseFloat(contractPrice)) * 100).toFixed(2) -
+    (parseFloat(stockPrice) * 100).toFixed(2)
   );
 };
